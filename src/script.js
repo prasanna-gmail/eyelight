f9d301bbe31444653b5339a93abc65aa8e42f9b7
PrasannaP <mail2lulu@gmail.com>
2023-07-07 19:44:51 +0530

    reflection working 1 for beam1

--- src/script.js
+++ src/script.js
@@ -64,29 +64,29 @@ scene.add(sunLight);
 //All object
 var Geometry, Material;
 var objectArray = [];
-    Geometry = new THREE.BoxGeometry(4, 8, 10);
-    Material = new THREE.MeshPhongMaterial({
-        color: 0x00ff00
-    });
-    var meshRefletor1 = new THREE.Mesh(Geometry, Material);
+Geometry = new THREE.BoxGeometry(4, 8, 10);
+Material = new THREE.MeshPhongMaterial({
+    color: 0x00ff00
+});
+var meshRefletor1 = new THREE.Mesh(Geometry, Material);
 
-    meshRefletor1.position.set(
-        20,
-        0,
-        -6
-    );
-    objectArray.push(meshRefletor1);
-    scene.add(meshRefletor1);
+meshRefletor1.position.set(
+    20,
+    0,
+    -6
+);
+objectArray.push(meshRefletor1);
+scene.add(meshRefletor1);
 
-    var meshRefletor2 = new THREE.Mesh(Geometry, Material);
+var meshRefletor2 = new THREE.Mesh(Geometry, Material);
 
-    meshRefletor2.position.set(
-        25,
-        0,
-        -6
-    );
-    objectArray.push(meshRefletor2);
-    scene.add(meshRefletor2);
+meshRefletor2.position.set(
+    25,
+    0,
+    -6
+);
+objectArray.push(meshRefletor2);
+scene.add(meshRefletor2);
 
 var LaserBeam1 = new LaserBeam({
     reflectMax: 5
@@ -130,28 +130,28 @@ thLaserbeam.onValuesChange((values) => {
 
     LaserBeam1.object3d.position.set(values.position.x, values.position.y, values.position.z);
     // LaserBeam1.object3d.position.set(values.intersect.x , values.intersect.y , values.intersect.z )
-    
+
     LaserBeam1.intersect(
 
         new THREE.Vector3(values.intersect.x, values.intersect.y, values.intersect.z), objectArray
     );
-   //LaserBeam1.object3d.scale.z=  values.scale.z;
-   config.length=  values.scale.z;
+    //LaserBeam1.object3d.scale.z=  values.scale.z;
+    config.length = values.scale.z;
     //LaserBeam1.hiddenReflectObject();
-  
+
 })
-const thReflector1 = sheet.object('Reflector1',{
-    position:types.compound({
+const thReflector1 = sheet.object('Reflector1', {
+    position: types.compound({
         x: types.number(meshRefletor1.position.x, { range: [-50, 50] }),
         y: types.number(meshRefletor1.position.y, { range: [-50, 50] }),
         z: types.number(meshRefletor1.position.z, { range: [-50, 50] })
     }),
-    rotation:types.compound({
-        x: types.number(meshRefletor1.rotation.x, { range: [-50, 50] }),
-        y: types.number(meshRefletor1.rotation.y, { range: [-50, 50] }),
-        z: types.number(meshRefletor1.rotation.z, { range: [-50, 50] })
+    rotation: types.compound({
+        x: types.number(meshRefletor1.rotation.x, { range: [-10, 10] }),
+        y: types.number(meshRefletor1.rotation.y, { range: [-10, 10] }),
+        z: types.number(meshRefletor1.rotation.z, { range: [-10, 10] })
     }),
-    scale:types.compound({
+    scale: types.compound({
         x: types.number(meshRefletor1.scale.x, { range: [0, 10] }),
         y: types.number(meshRefletor1.scale.y, { range: [0, 10] }),
         z: types.number(meshRefletor1.scale.z, { range: [0, 10] })
@@ -161,21 +161,21 @@ thReflector1.onValuesChange((values) => {
     meshRefletor1.position.set(values.position.x, values.position.y, values.position.z)
     meshRefletor1.rotation.set(values.rotation.x, values.rotation.y, values.rotation.z)
     meshRefletor1.scale.set(values.scale.x, values.scale.y, values.scale.z)
- 
- })
 
- const thReflector2 = sheet.object('Reflector2',{
-    position:types.compound({
+})
+
+const thReflector2 = sheet.object('Reflector2', {
+    position: types.compound({
         x: types.number(meshRefletor2.position.x, { range: [-50, 50] }),
         y: types.number(meshRefletor2.position.y, { range: [-50, 50] }),
         z: types.number(meshRefletor2.position.z, { range: [-50, 50] })
     }),
-    rotation:types.compound({
-        x: types.number(meshRefletor2.rotation.x, { range: [-50, 50] }),
-        y: types.number(meshRefletor2.rotation.y, { range: [-50, 50] }),
-        z: types.number(meshRefletor2.rotation.z, { range: [-50, 50] })
+    rotation: types.compound({
+        x: types.number(meshRefletor2.rotation.x, { range: [-1, 1] }),
+        y: types.number(meshRefletor2.rotation.y, { range: [-1, 1] }),
+        z: types.number(meshRefletor2.rotation.z, { range: [-1, 1] })
     }),
-    scale:types.compound({
+    scale: types.compound({
         x: types.number(meshRefletor2.scale.x, { range: [0, 10] }),
         y: types.number(meshRefletor2.scale.y, { range: [0, 10] }),
         z: types.number(meshRefletor2.scale.z, { range: [0, 10] })
@@ -185,26 +185,26 @@ thReflector2.onValuesChange((values) => {
     meshRefletor2.position.set(values.position.x, values.position.y, values.position.z)
     meshRefletor2.rotation.set(values.rotation.x, values.rotation.y, values.rotation.z)
     meshRefletor2.scale.set(values.scale.x, values.scale.y, values.scale.z)
- 
- })
 
-const thCamera = sheet.object('Cemera',{
+})
+
+const thCamera = sheet.object('Cemera', {
     position: types.compound({
-        x:types.number(camera.position.x, { range: [-100, 100] }),
-        y:types.number(camera.position.y, { range: [-100, 100] }),
-        z:types.number(camera.position.z, { range: [-100, 100] }),
+        x: types.number(camera.position.x, { range: [-100, 100] }),
+        y: types.number(camera.position.y, { range: [-100, 100] }),
+        z: types.number(camera.position.z, { range: [-100, 100] }),
     }),
     lookAt: types.compound({
-        x:types.number( 0,{ range: [-100, 100] }),
-        y:types.number(0, { range: [-100, 100] }),
-        z:types.number(0,{ range: [-100, 100] }),
+        x: types.number(0, { range: [-100, 100] }),
+        y: types.number(0, { range: [-100, 100] }),
+        z: types.number(0, { range: [-100, 100] }),
     })
 
 
 })
 thCamera.onValuesChange((values) => {
-   camera.position.set(values.position.x,values.position.y,values.position.z)
-   camera.lookAt(values.lookAt.x,values.lookAt.y,values.lookAt.z)
+    camera.position.set(values.position.x, values.position.y, values.position.z)
+    camera.lookAt(values.lookAt.x, values.lookAt.y, values.lookAt.z)
 
 })
 
@@ -216,8 +216,8 @@ thCamera.onValuesChange((values) => {
 function animate() {
 
     requestAnimationFrame(animate);
-//    LaserBeam1.object3d.position.set(-6, -101, -120);
-//LaserBeam1.object3d.position.set(-6, -100, -120);
+    //    LaserBeam1.object3d.position.set(-6, -101, -120);
+    //LaserBeam1.object3d.position.set(-6, -100, -120);
     // LaserBeam1.intersect(
 
     //     new THREE.Vector3(-8, 1, -10), objectArray
@@ -241,7 +241,7 @@ var config = {
 
 function LaserBeam(iconfig) {
 
-    
+
     config = $.extend(config, iconfig);
 
     this.object3d = new THREE.Object3D();
@@ -261,8 +261,8 @@ function LaserBeam(iconfig) {
         depthWrite: false,
         transparent: true
     });
-     var geometry = new THREE.PlaneGeometry(1, 0.1 * 5);
-  //  var geometry = new THREE.PlaneGeometry(1,9);
+    var geometry = new THREE.PlaneGeometry(1, 0.1 * 5);
+    //  var geometry = new THREE.PlaneGeometry(1,9);
     geometry.rotateY(0.5 * Math.PI);
 
     //use planes to simulate laserbeam
@@ -327,7 +327,7 @@ function LaserBeam(iconfig) {
         }
         //non collision
         else {
-            console.log("config.length--->",config.length)
+            console.log("config.length--->", config.length)
             this.object3d.scale.z = config.length;
             this.pointLight.visible = false;
             this.object3d.lookAt(
@@ -407,47 +407,47 @@ document.onkeydown = function (e) {
 };
 
 // Load a glTF resource
-function loadGLTF(){
+function loadGLTF() {
     loader.load(
         // resource URL
         'assets/humaneye.glb',
         // called when the resource is loaded
         function (gltf) {
-    
-    
+
+
             eyeScene = gltf.scene;
             console.log("pkp:  ~ file: script.js:258 ~ eyeScene:", eyeScene)
             eyeScene.position.set(-110, -100, -40);
-    
+
             eyeScene.rotation.set(1.8, 4.8, 8.58);
-    
+
             eyeScene.scale.x = 0.26;
             eyeScene.scale.y = 0.26;
             eyeScene.scale.z = 0.26;
-    
-    
-    
-    
+
+
+
+
             scene.add(gltf.scene);
-    
+
             gltf.animations; // Array<THREE.AnimationClip>
             gltf.scene; // THREE.Group
             gltf.scenes; // Array<THREE.Group>
             gltf.cameras; // Array<THREE.Camera>
             gltf.asset; // Object
-    
+
         },
         // called while loading is progressing
         function (xhr) {
-    
+
             console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-    
+
         },
         // called when loading has errors
         function (error) {
-    
+
             console.log('An error happened');
-    
+
         }
     );
 }
